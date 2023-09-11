7032903774ed3c6aa1c126dd8b109d10129626b0
Huynh Chi <huynhchi90524@gmail.com>
2023-09-11 21:41:07 +0700

    video 10.2.1 : validate form register react

--- src/components/Register/RegisterContentRight/RegisterContentRight.js
+++ src/components/Register/RegisterContentRight/RegisterContentRight.js
@@ -1,14 +1,156 @@
-import './RegisterContentRight.scss';
 import { useNavigate } from 'react-router-dom';
+// import axios from 'axios';
+
+import './RegisterContentRight.scss';
+import { useEffect, useState, useRef } from 'react';
 
 function RegisterContentRight() {
-   // hook de dieu huong trang cua react-router-dom
-   const navigate = useNavigate();
+   const [email, setEmail] = useState('');
+   const [phone, setPhone] = useState('');
+   const [username, setUsername] = useState('');
+   const [password, setPassword] = useState('');
+   const [confirmPassword, setConfirmPassword] = useState('');
+   const [validEmail, setValidEmail] = useState(true);
+   const [validPassword, setValidPassword] = useState(true);
+   const [validConfirmPassword, setValidConfirmPassword] = useState(true);
 
+   const emailRef = useRef();
+   const phoneRef = useRef();
+   const usernameRef = useRef();
+   const passwordRef = useRef();
+   const confirmPasswordRef = useRef();
+
+   const allRef = [
+      emailRef.current,
+      phoneRef.current,
+      usernameRef.current,
+      passwordRef.current,
+      confirmPasswordRef.current,
+   ];
+
+   // hook de dieu huong trang cua 'react-router-dom'
+   const navigate = useNavigate();
    const handleLoginPage = () => {
       navigate('/login');
    };
 
+   // call API
+   useEffect(() => {
+      // axios.get('http://localhost:1997/api/test-api').then((data) => {
+      //    console.log(data);
+      // });
+   }, []);
+
+   // hien thi warning
+   const warnInput = (input) => {
+      input.closest('.input-wrapper').className = 'input-wrapper form-warning';
+   };
+
+   // off warning
+   const unwarnInput = (input) => {
+      input.closest('.input-wrapper').className = 'input-wrapper';
+   };
+
+   // onChange inputs
+   const handleChangeInputValue = (event, setState, inputRef) => {
+      setState(event.target.value);
+      unwarnInput(inputRef);
+   };
+
+   // check input value khi blur khoi input
+   const handleCheckValue = (inputRef, checkValid) => {
+      if (!inputRef.value) {
+         warnInput(inputRef);
+      } else {
+         unwarnInput(inputRef);
+      }
+
+      if (checkValid) {
+         checkValid();
+      }
+   };
+
+   // clear input values sau khi submit thanh cong
+   const clearInputValues = () => {
+      setEmail('');
+      setPhone('');
+      setUsername('');
+      setPassword('');
+      setConfirmPassword('');
+   };
+
+   // check valid Email
+   const checkValidEmail = () => {
+      if (email !== '') {
+         const regex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+         if (!regex.test(email)) {
+            setValidEmail(false);
+            warnInput(emailRef.current);
+         } else {
+            setValidEmail(true);
+            unwarnInput(emailRef.current);
+         }
+      }
+   };
+
+   // check valid password
+   const checkValidPassword = () => {
+      if (password !== '') {
+         if (password.length < 6) {
+            setValidPassword(false);
+            warnInput(passwordRef.current);
+         } else {
+            setValidPassword(true);
+            unwarnInput(passwordRef.current);
+         }
+      }
+   };
+
+   // check corret confirm password
+   const checkConfirmPassword = () => {
+      if (validPassword && confirmPassword !== '') {
+         if (confirmPassword !== password) {
+            setValidConfirmPassword(false);
+            warnInput(confirmPasswordRef.current);
+         } else {
+            setValidConfirmPassword(true);
+            unwarnInput(confirmPasswordRef.current);
+         }
+      }
+   };
+
+   // xu ly submit
+   const handleRegister = (e) => {
+      e.preventDefault();
+
+      // khúc này vừa mounted thì allRef items nó ra undefined, nên phải check khi submit
+      const checkInputUndefined = allRef.every((ref) => ref === undefined);
+
+      if (!checkInputUndefined) {
+         // check required từng input để warning
+         allRef.forEach((ref) => {
+            handleCheckValue(ref);
+         });
+
+         // check valid Email, PW, confimPW
+         checkValidEmail();
+         checkValidPassword();
+         checkConfirmPassword();
+
+         // check required all
+         let isFillAll = allRef.every((ref) => {
+            return ref.value !== '' && ref.value !== 'undefined';
+         });
+
+         if (isFillAll && validEmail && validPassword && validConfirmPassword) {
+            const inputData = { email, phone, username, password };
+            console.log('show inputData: ', inputData);
+            clearInputValues();
+         }
+      } else {
+         document.querySelectorAll('.form-input').forEach((item) => warnInput(item));
+      }
+   };
    return (
       <div className="register-content-right">
          <div className="form-register">
@@ -17,27 +159,73 @@ function RegisterContentRight() {
             </div>
 
             <div className="input-wrapper">
-               <input className="form-input" type="text" placeholder="Email address" />
+               {!validEmail && <div className="pop-warning">Invalid email!</div>}
+
+               <input
+                  className="form-input"
+                  type="text"
+                  placeholder="Email address"
+                  ref={emailRef}
+                  value={email}
+                  onChange={(e) => handleChangeInputValue(e, setEmail, emailRef.current)}
+                  onBlur={() => handleCheckValue(emailRef.current, checkValidEmail)}
+               />
             </div>
 
             <div className="input-wrapper">
-               <input className="form-input" type="text" placeholder="Phone number" />
+               <input
+                  className="form-input"
+                  type="text"
+                  placeholder="Phone number"
+                  ref={phoneRef}
+                  value={phone}
+                  onChange={(e) => handleChangeInputValue(e, setPhone, phoneRef.current)}
+                  onBlur={() => handleCheckValue(phoneRef.current)}
+               />
             </div>
 
             <div className="input-wrapper">
-               <input className="form-input" type="text" placeholder="Your name" />
+               <input
+                  className="form-input"
+                  type="text"
+                  placeholder="Your name"
+                  ref={usernameRef}
+                  value={username}
+                  onChange={(e) => handleChangeInputValue(e, setUsername, usernameRef.current)}
+                  onBlur={() => handleCheckValue(usernameRef.current)}
+               />
             </div>
 
             <div className="input-wrapper">
-               <input className="form-input" type="password" placeholder="Password" />
+               {!validPassword && <div className="pop-warning">Password must be longer then 6!</div>}
+               <input
+                  className="form-input"
+                  type="password"
+                  placeholder="Password"
+                  ref={passwordRef}
+                  value={password}
+                  onChange={(e) => handleChangeInputValue(e, setPassword, passwordRef.current)}
+                  onBlur={() => handleCheckValue(passwordRef.current, checkValidPassword)}
+               />
             </div>
 
             <div className="input-wrapper">
-               <input className="form-input" type="password" placeholder="Confirm your password" />
+               {!validConfirmPassword && <div className="pop-warning">Incorrect password!</div>}
+               <input
+                  className="form-input"
+                  type="password"
+                  placeholder="Confirm your password"
+                  ref={confirmPasswordRef}
+                  value={confirmPassword}
+                  onChange={(e) => handleChangeInputValue(e, setConfirmPassword, confirmPasswordRef.current)}
+                  onBlur={() => handleCheckValue(confirmPasswordRef.current, checkConfirmPassword)}
+               />
             </div>
 
             <div className="register-btn-wrapper">
-               <button className="register-btn">Register</button>
+               <button type="submit" className="register-btn" onClick={handleRegister}>
+                  Register
+               </button>
             </div>
 
             <hr className="big-line" />
